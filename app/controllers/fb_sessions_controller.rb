df8254e676e006839b4a58c3f3fa45da357991f8
Arielle Chen & Arielle Chen <pair+achen116.achen116@devbootcamp.com>
2015-06-25 16:46:28 -0700

    test fb get route

--- app/controllers/fb_sessions_controller.rb
+++ app/controllers/fb_sessions_controller.rb
@@ -1,5 +1,4 @@
 # facebook sign in ====================================================
-get '/facebook' do
+get '/facebook_login' do
   #redirect to facebook authorization url
-  p "in /facebook route!"
 end
