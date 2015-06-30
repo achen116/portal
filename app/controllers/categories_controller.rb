ec5796817cc8bbdbc69a921b6f8fab2dc2eaef70
achen116 <chen.arielle@gmail.com>
2015-06-30 11:37:55 -0700

    clean up p debugging statements

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,10 +10,6 @@ end
 get '/categories/:id' do
   category = Category.find(params[:id])
   products = Product.where(category_id: params[:id])
-  
-  p "*" * 100
-  p params
-  p "*" * 100
 
   content_type :html
 	erb :'/categories/single_category', layout: false, locals: {category: category, products: products}
